bc46a3e855cf028e23a7bcb9bc253023cfda99c9
Vladimer Kobayashi <vladimer.kobayashi@gmail.com>
2026-07-21 14:08:24 +0200

    create template for folder structure added

---
+++ template.sh
@@ -0,0 +1,17 @@
+# Create directory structure
+mkdir -p src
+mkdir -p research
+
+#Create files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.ph
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+
+
+echo "Directory and files created successfully!."
+
