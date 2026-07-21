fb4c7d7fb32ff5dad88b39ea6445ba02b47ecfdb
Vladimer Kobayashi <vladimer.kobayashi@gmail.com>
2026-07-21 15:04:48 +0200

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author ="Vladimer Kobayashi",
+    author_email ="vladimer.kobayashi@gmail.com",
+    packages = find_packages(),
+    install_requires=[]
+)
