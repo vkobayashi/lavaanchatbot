b8d9362c0330894e8b7eb3f7a6aceb92369d9049
Vladimer Kobayashi <vladimer.kobayashi@gmail.com>
2026-07-23 18:24:53 +0200

    modular

---
+++ src/helper.py
@@ -0,0 +1,48 @@
+from langchain.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings
+from typing import List
+from langchain.schema import Document
+
+def load_pdf_files(data):
+    loader = DirectoryLoader(
+        data,
+        glob="*.pdf",
+        loader_cls=PyPDFLoader
+    )
+    documents = loader.load()
+    return documents
+
+def filter_to_minimal_docs(docs: List[Document]) -> List[Document]:
+    """
+    Given a list of Document objects, return a new list of Document objects
+    containing only 'source' in metadata and the original page_content.
+    """
+    minimal_docs: List[Document] = []
+    for doc in docs:
+        src = doc.metadata.get("source")
+        minimal_docs.append(
+            Document(
+                page_content=doc.page_content,
+                metadata={"source":src}
+            )
+        )
+    return minimal_docs
+
+def text_split(minimal_docs):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=500,
+        chunk_overlap=20
+    )
+    texts_chunk = text_splitter.split_documents(minimal_docs)
+    return texts_chunk
+
+
+def download_embeddings():
+
+    model_name = "sentence-transformers/all-MiniLM-L6-v2"
+    embeddings = HuggingFaceEmbeddings(
+        model_name = model_name
+       
+    )
+    return embeddings
